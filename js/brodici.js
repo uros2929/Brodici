2e8ba22694194a5e63c6a4bf091c5feb1b3a5e92
ZarkoZivojnovic <zivojnovic@gmail.com>
2018-05-10 23:13:52 +0200

    zavrseno pozicioniranje

--- js/brodici.js
+++ js/brodici.js
@@ -1,4 +1,5 @@
-const labels = {emptySpace: 0, ship: 1, reservedSpace: 2, selectedShip:3};
+const labels = {emptySpace: 0, ship: 1, reservedSpace: 2, selectedShip: 3};
+
 let player1 = {
         name: "",
         table: createTable(),
@@ -12,16 +13,49 @@ let player1 = {
     ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1],
     input = document.getElementById("start");
 
-input.addEventListener("submit", (event)=>{
+input.addEventListener("submit", (event) => {
     player1.name = document.getElementById("username").value;
     input.style.display = "none";
     document.getElementById("table").style.display = "flex";
     document.getElementById("playerOne").textContent = player1.name;
-    startGame();
+    positionSetting();
     event.preventDefault();
 });
 
-function startGame() {
+let allListeners = {
+    settingPosition: {
+        mouseDown: function (event) {
+            let targetProperties = tableSetup.createTargetProperties(event);
+            localStorage.setItem("property", JSON.stringify(targetProperties));
+            player1.table = tableSetup.colorSelectedShip(player1.table, player1.shipsPosition, targetProperties, "on");
+        },
+        mouseUp: function (event) {
+            let targetProperties = JSON.parse(localStorage.getItem("property")),
+                newTableAndPosition = tableSetup.doSomething(player1.table, player1.shipsPosition, event, targetProperties);
+            player1.table = tableSetup.colorSelectedShip(player1.table, player1.shipsPosition, targetProperties, "off");
+            player1.table = newTableAndPosition.table;
+            player1.shipsPosition = newTableAndPosition.shipPosition;
+            localStorage.removeItem("property");
+        },
+        newRandomPosition: function () {
+            let random = tableSetup.randomShipsPosition(player1.table);
+            player1.table = random.myTable;
+            player1.shipsPosition = random.shipsOnTable;
+        },
+        confirmPosition: function () {
+            /*ajaxStorage.set(player1.name, player1.table, function () {
+            console.log("set table", player1.table);
+        });*/
+            startTheBattle();
+        }
+    }
+};
+
+function startTheBattle() {
+
+}
+
+function positionSetting() {
     let random = tableSetup.randomShipsPosition(player1.table);
     player1.table = random.myTable;
     player1.shipsPosition = random.shipsOnTable;
@@ -29,21 +63,18 @@ function startGame() {
         document.getElementById("player1").innerHTML = drawOnPage(player1.table, "Player1").outerHTML;
         document.getElementById("player2").innerHTML = drawOnPage(player2.table, "Player2").outerHTML;
     }, 100);
-    turnOnListeners("player1");
+    listenersForPositionSetting("player1");
 }
 
-function turnOnListeners(id) {
+function listenersForPositionSetting(id) {
     let player = document.getElementById(id);
-    player.addEventListener('contextmenu', (event) => {event.preventDefault();}, false);
-    player.addEventListener("mousedown", (event) => {
-        let targetProperties = tableSetup.createTargetProperties(event);
-        localStorage.setItem("property", JSON.stringify(targetProperties));
-    });
-    player.addEventListener("mouseup", (event) => {
-        let targetProperties = JSON.parse(localStorage.getItem("property"));
-        tableSetup.doSomething(event, targetProperties);
-        localStorage.removeItem("property");
-    });
+    player.addEventListener('contextmenu', (event) => {
+        event.preventDefault();
+    }, false);
+    player.addEventListener("mousedown", allListeners.settingPosition.mouseDown);
+    player.addEventListener("mouseup", allListeners.settingPosition.mouseUp);
+    document.getElementById("newRandom").addEventListener("click", allListeners.settingPosition.newRandomPosition);
+    document.getElementById("confirmPosition").addEventListener("click", allListeners.settingPosition.confirmPosition);
 }
 
 function createTable() {
@@ -85,3 +116,4 @@ function koIgraPrvi() {
         return player2;
     }
 }
+
--- js/tableSetup.js
+++ js/tableSetup.js
@@ -23,41 +23,54 @@ let tableSetup = {
         return targetProperties;
     },
 
-    colorSelectedShip: function (table, targetProperties, turnOnOrOff) {
-        let type,
-            myTable = table;
+    colorSelectedShip: function (table, shipPosition, targetProperties, turnOnOrOff) {
+        let type;
         turnOnOrOff === "on" ? type = labels.selectedShip : type = labels.ship;
-        let selectedShip = player1.shipsPosition[targetProperties.shipNumber].position;
+        let selectedShip = shipPosition[targetProperties.shipNumber].position;
         for (let field = 0; field < selectedShip.length; field++) {
             for (let index = 0; index < selectedShip[field].length; index++) {
-                myTable[selectedShip[field][0]][selectedShip[field][1]] = type;
+                table[selectedShip[field][0]][selectedShip[field][1]] = type;
             }
         }
-        return myTable;
+        return table;
     },
 
-    rotateOrMoveShip: function (event, targetProperties, rotateOrMove) {
+    rotateOrMoveShip: function (table, shipPosition, event, targetProperties, rotateOrMove) {
+        console.log("rotateOrMoveShip");
         if (rotateOrMove === "rotate" ){
             targetProperties.newPosition = this.createNewRotatedPosition(targetProperties)
         } else if(rotateOrMove === "move") {
             targetProperties.newPosition = this.createNewMovedPosition(event, targetProperties);
         }
-        this.removeOldPosition(targetProperties);
-        if (this.isPositionEmpty(targetProperties) === true) {
-            this.addTranslatedShip(targetProperties, rotateOrMove);
+        let newTableAndShip = this.removeOldPosition(table, shipPosition, targetProperties);
+        table = newTableAndShip.table;
+        shipPosition = newTableAndShip.shipPosition;
+        if (this.isPositionEmpty(table, targetProperties) === true) {
+            let newTableAndShip = this.addTranslatedShip(table, shipPosition, targetProperties, rotateOrMove);
+            table = newTableAndShip.table;
+            shipPosition = newTableAndShip.shipPosition;
+            console.log("translated returned");
         } else {
-            this.returnOldPosition(targetProperties);
+            let oldTableAndShip = this.returnOldPosition(table, shipPosition, targetProperties);
+            table= oldTableAndShip.table;
+            shipPosition = oldTableAndShip.shipPosition;
+            console.log("old returned");
         }
+        return {table, shipPosition};
     },
 
-    doSomething: function (event, targetProperties) {
+    doSomething: function (table, shipPosition, event, targetProperties) {
+        console.log("doSomething");
         let rotateOrMove;
         if (event.button === 0) {
             rotateOrMove = "move";
         } else if (event.button === 2) {
             rotateOrMove = "rotate";
         }
-        this.rotateOrMoveShip(event, targetProperties, rotateOrMove);
+        let tableAndShip = this.rotateOrMoveShip(table, shipPosition, event, targetProperties, rotateOrMove);
+        table = tableAndShip.table;
+        shipPosition = tableAndShip.shipPosition;
+        return {table, shipPosition};
     },
 
     createNewMovedPosition: function (event, targetProperties) {
@@ -100,32 +113,31 @@ let tableSetup = {
         return newPosition;
     },
 
-    addTranslatedShip: function (obj, rotateOrMove) {
-        let table = player1.table;
+    addTranslatedShip: function (table,shipPosition, obj, rotateOrMove) {
         let newDirection;
         if (rotateOrMove === "rotate") {
             obj.oldDirection === "vertical" ? newDirection = "horizontal" : newDirection = "vertical";
         } else {
             newDirection = obj.oldDirection;
         }
-        player1.shipsPosition[obj.shipNumber] = {
+        shipPosition[obj.shipNumber] = {
             position: obj.newPosition,
             direction: newDirection
         };
         for (let cell = 0; cell < obj.newPosition.length; cell++) {
             table[obj.newPosition[cell][0]][obj.newPosition[cell][1]] = labels.ship;
         }
-        player1.table = table;
-        this.updateReservedSpace(player1.table);
+        table = this.updateReservedSpace(table);
+        return {table, shipPosition};
     },
 
-    removeOldPosition: function (obj) {
-        delete player1.shipsPosition[obj.shipNumber];
+    removeOldPosition: function (table,shipPosition, obj) {
+        delete shipPosition[obj.shipNumber];
         for (let cell = 0; cell < obj.oldPosition.length; cell++) {
-            player1.table[obj.oldPosition[cell][0]][obj.oldPosition[cell][1]] = labels.emptySpace;
+            table[obj.oldPosition[cell][0]][obj.oldPosition[cell][1]] = labels.emptySpace;
         }
-        this.updateReservedSpace(player1.table);
-
+        table = this.updateReservedSpace(table);
+        return {table, shipPosition}
     },
 
     updateReservedSpace: function (table) {
@@ -138,48 +150,48 @@ let tableSetup = {
         for (let ship in player1.shipsPosition) { //add new reserved space
             let shipPosition = player1.shipsPosition[ship]["position"];
             for (let position = 0; position < shipPosition.length; position++) {
-                this.addReservedSpace(table, shipPosition[position]);
+                table = this.addReservedSpace(table, shipPosition[position]);
             }
         }
+        return table;
     },
 
-    returnOldPosition: function (obj) {
-        let myTable = player1.table;
-        player1.shipsPosition[obj.shipNumber] = {
+    returnOldPosition: function (table, shipPosition, obj) {
+       shipPosition[obj.shipNumber] = {
             position: obj.oldPosition,
             direction: obj.oldDirection
         };
         for (let cell = 0; cell < obj.oldPosition.length; cell++) {
-            myTable[obj.oldPosition[cell][0]][obj.oldPosition[cell][1]] = labels.ship;
+            table[obj.oldPosition[cell][0]][obj.oldPosition[cell][1]] = labels.ship;
         }
-        player1.table = myTable;
-        this.updateReservedSpace(player1.table);
+        table = this.updateReservedSpace(table);
+        return {table, shipPosition};
     },
 
-    isPositionEmpty: function (obj) {
+    isPositionEmpty: function (table, obj) {
         let counter = 0;
         if (typeof obj.newPosition === "undefined") return false;
         for (let cell = 0; cell < obj.newPosition.length; cell++) {
             let index = obj.newPosition[cell];
-            if (typeof player1.table[index[0]][index[1]] === "undefined") {
+            if (typeof table[index[0]][index[1]] === "undefined") {
                 return false;
             }
-            if (player1.table[index[0]][index[1]] === labels.emptySpace)
+            if (table[index[0]][index[1]] === labels.emptySpace)
                 counter++;
         }
         return obj.newPosition.length === counter;
     },
 
     addReservedSpace: function (table, cell) {
-        let myTable = table;
         for (let verticalPosition = -1; verticalPosition < 2; verticalPosition++) {
             for (let horizontalPosition = -1; horizontalPosition < 2; horizontalPosition++) {
-                if (myTable[cell[0] + verticalPosition] === undefined || myTable[cell[0] + verticalPosition][cell[1] + horizontalPosition] === undefined) continue;
-                if (myTable[cell[0] + verticalPosition][cell[1] + horizontalPosition] !== labels.ship) {
-                    myTable[cell[0] + verticalPosition][cell[1] + horizontalPosition] = labels.reservedSpace;
+                if (table[cell[0] + verticalPosition] === undefined || table[cell[0] + verticalPosition][cell[1] + horizontalPosition] === undefined) continue;
+                if (table[cell[0] + verticalPosition][cell[1] + horizontalPosition] !== labels.ship) {
+                    table[cell[0] + verticalPosition][cell[1] + horizontalPosition] = labels.reservedSpace;
                 }
             }
         }
+        return table;
     },
 
     randomShipsPosition: function (table) {
